6a5bc8886e1f11152b521d4436a62600cf9d2353
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-10-23 17:06:08 -0400

    linkks

--- docs/Module Reference/Desci Nodes.md
+++ docs/Module Reference/Desci Nodes.md
@@ -3,3 +3,16 @@ sidebar_position: 2
 ---
 
 # Desci Nodes
+
+[Desci Nodes](https://nodes.desci.com/web) are an offering from [Desci Labs](https://desci.com/)  that packages scientific content in research objects that provides a higher degree of composability and provenance than just pdfs from a publishing house.
+
+
+## Command Reference
+
+| Command | Args | Output |
+| `dpidFetch` | dpid | Canvas Dag | 
+
+## Usage
+
+
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,15 +41,11 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -78,7 +74,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/Obsidian-Desci',
             label: 'GitHub',
             position: 'right',
           },
@@ -100,16 +96,12 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                href: 'https://discord.gg/uTyEKVHTb4',
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/Obsidian_Desci',
               },
             ],
           },
@@ -122,12 +114,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/Obsidian-Desci',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Obsidian-desci, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Obsidian-desci. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
